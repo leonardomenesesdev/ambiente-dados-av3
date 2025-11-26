cf97c4301a461fb031b605a8dd7dbbae703fd6af
leonardomenesesdev <leonardomenesesc@edu.unifor.br>
2025-11-26 15:51:08 -0300

    ajustes finais

--- PopulateFichaTreino.js
+++ PopulateFichaTreino.js
@@ -4,9 +4,10 @@ import { faker } from "@faker-js/faker";
 const total = 30;
 const chunk = 1000;
 
-function gerarFichaTreino() {
+function gerarFichaTreino(idAluno) {
     return [
-      faker.person.firstName()
+        faker.person.firstName(),  
+        idAluno                     
     ];
 }
 
@@ -15,36 +16,49 @@ async function popular() {
     console.log("Conectando ao banco");
 
     await conn.beginTransaction();
-    console.log('Iniciando a populaçao de fichas de treino');
+    console.log("Carregando IDs reais dos alunos...");
 
     try {
+        const [rows] = await conn.query("SELECT id_aluno FROM Aluno");
+        const alunos = rows.map(r => r.id_aluno);
+
+        if (alunos.length === 0) {
+            throw new Error("Nenhum aluno encontrado!");
+        }
+
+        console.log(`Foram encontrados ${alunos.length} alunos (IDs reais).`);
+        console.log("Iniciando a população de fichas de treino...");
+
         for (let i = 0; i < total; i += chunk) {
 
             const valores = [];
             const fim = Math.min(i + chunk, total);
 
             for (let j = i; j < fim; j++) {
-                valores.push(gerarFichaTreino());
+                valores.push(
+                    gerarFichaTreino(
+                        faker.helpers.arrayElement(alunos) 
+                    )
+                );
             }
 
-            // Agora sim: um único INSERT por chunk
-            const placeholders = valores.map(() => '(?)').join(', ');
+            const placeholders = valores.map(() => '(?, ?)').join(', ');
             const flat = valores.flat();
 
             await conn.query(
-              `
+                `
                 INSERT INTO FichaTreino 
-                (titulo)
+                (titulo, id_aluno)
                 VALUES ${placeholders}
             `,
-              flat
+                flat
             );
 
             console.log(`Inseridos ${fim} de ${total} fichas de treino`);
         }
 
         await conn.commit();
-        console.log("População de fichas de treino finalizada");
+        console.log("População de fichas de treino finalizada!");
 
     } catch (error) {
         console.error("Erro ao popular ficha de treino:", error);
